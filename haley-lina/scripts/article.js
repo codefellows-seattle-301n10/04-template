39f53934b273cbd9bcb32aa3c2021d344948118c
Laboni A <asthanal@yahoo.com>
2018-03-24 15:38:04 -0700

    author drop down working

--- haley-lina/scripts/article.js
+++ haley-lina/scripts/article.js
@@ -13,7 +13,7 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // TODONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-  let template = $('#articles-template').html()
+  let template = $('#article-template').html()
   let templateRender = Handlebars.compile(template);
   // REVIEWED: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
@@ -22,7 +22,7 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
 
-  this.publishStatus = this.publishedOn ? `published about ${this.daysAgo} days ago` : '(draft)';
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // REVIEWED: The ternary operator above accomplishes this same logic.
   // if(this.publishedOn) {
