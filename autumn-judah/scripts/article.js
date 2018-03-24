04264edcaa3d4c8d9ba8d4276b53faea89ab458a
SeatownAC <33205876+SeatownAC@users.noreply.github.com>
2018-03-24 13:59:54 -0700

    cleaned up file

--- autumn-judah/scripts/article.js
+++ autumn-judah/scripts/article.js
@@ -13,21 +13,8 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
- judah-autumn
-  let template = $('#article-template').html();
-  let templateRender = Handlebars.compile(template);
-
-  let html = templateRender(this);
-
-  return html;
-
-};
-
-
-
-
+ 
   // REVIEWED: If your template will use properties that aren't on the object yet, add them.
-
   let template = $('#articles-template').html();
   let templateRender = Handlebars.compile(template);
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
