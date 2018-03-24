6ac27d3c3410b8e8a227015bd6ab18d5b23fba70
Mrebb <madhu.rebbana@gmail.com>
2018-03-24 15:51:41 -0700

    arrow functions and handlebar template building

--- madhu-jesse/scripts/article.js
+++ madhu-jesse/scripts/article.js
@@ -13,14 +13,11 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // DONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-    let template = Handlebars.compile($('#article-template').text());
+  let template = Handlebars.compile($('#article-template').text());
   
-    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-    this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   
-    
-    return template(this);
-  };
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -36,13 +33,13 @@ Article.prototype.toHtml = function() {
   // } else {
   //   this.publishStatus = '(draft)';
   // }
+  // TODONE: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+  return template(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// Because sort method expects two parameters as arguments which has to be enclosed in parantheses in case of Arrow functions.
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
