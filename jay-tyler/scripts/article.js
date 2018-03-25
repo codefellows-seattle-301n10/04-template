664d19b80265a5c3b22e64268079c4415fb98514
Tyler <Tconfalone@gmail.com>
2018-03-24 18:46:08 -0700

    README done

--- jay-tyler/scripts/article.js
+++ jay-tyler/scripts/article.js
@@ -18,7 +18,7 @@ Article.prototype.toHtml = function() {
 
   let theTemplate = Handlebars.compile(theTemplateScript);
 
-  // REVIEW: If your template will use properties that aren't on the object yet, add them.
+  // REVIEWed: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
